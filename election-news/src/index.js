c0cfe24d461de4ecae68ae2d042004693398689b
Alujan28 <kovialujan@gmail.com>
2024-11-05 12:09:46 +0530

    feat: add ElectionChart

--- election-news/src/index.js
+++ election-news/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import District from './Districts';
+import Chart from './ElectionChart.js';
+import ElectionChart from './ElectionChart.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <District />
+    <ElectionChart />
   </React.StrictMode>
 );
 
